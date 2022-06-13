032388e138dbeac2b141914c77c65a7e47d0eac7
Amirreza Madadi <amirrezamddi790@gmail.com>
2022-06-13 16:49:35 +0430

    Add html templates to the main Django template and create a template directory and create a static directory for css, js, font

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 from core.local_settings import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,7 +119,9 @@ AUTH_USER_MODEL = 'authenticating.CustomUser'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR , 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
