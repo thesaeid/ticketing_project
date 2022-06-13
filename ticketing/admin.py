8de259aa45d6277752b076f65b6254c68ae68907
Amirreza Madadi <amirrezamddi790@gmail.com>
2022-06-13 22:23:43 +0430

    Create a new table to post comments and change ticketing fields based on pages

--- ticketing/admin.py
+++ ticketing/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from ticketing.models import Ticheting , Category
 class TichetingAdmin(admin.ModelAdmin):
-    list_display = ['user' , 'title' , 'is_active' , 'description']
+    list_display = ['user' , 'title' , 'type' , 'description']
 
 
 admin.site.register(Ticheting , TichetingAdmin)
