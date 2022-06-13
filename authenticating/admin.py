1fa5b51b7e8dfefcdea023751f403c90d8930ce6
Amirreza Madadi <amirrezamddi790@gmail.com>
2022-06-13 23:59:10 +0430

    fix bug

--- authenticating/admin.py
+++ authenticating/admin.py
@@ -9,16 +9,16 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ('email', 'is_staff', 'is_active','address')
-    list_filter = ('email', 'is_staff', 'is_active','address')
+    list_display = ('email', 'is_staff', 'is_active')
+    list_filter = ('email', 'is_staff', 'is_active')
     fieldsets = (
-        (None, {'fields': ('email', 'password' ,  'address')}),
+        (None, {'fields': ('email', 'password' ,  )}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active','address')}
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
         ),
     )
     search_fields = ('email',)
