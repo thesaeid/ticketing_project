0c0850569ebecc56042cf25562dead0de5edd212
Erfan <erfankiani10@gmail.com>
2022-07-01 03:38:14 +0430

    admin_view_ticket form implementation

--- ticketing/forms.py
+++ ticketing/forms.py
@@ -1,8 +1,15 @@
 from django import forms
-from .models import Ticketing
+from .models import Ticketing,ResponseTicket
 
 
 class UserSubmitTicketForm(forms.ModelForm):
     class Meta:
         model = Ticketing
         fields = ("title", "phonnum", "description")
+
+
+
+class AdminViewTicketForm(forms.ModelForm):
+    class Meta:
+        model = ResponseTicket
+        fields = ("message",)
