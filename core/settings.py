39e154961311870f4f0bd12c25f5ad94f9e6092d
Erfan <erfankiani10@gmail.com>
2022-07-01 03:37:15 +0430

    add email settings

--- core/settings.py
+++ core/settings.py
@@ -131,3 +131,14 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+
+
+#Email settings
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
+EMAIL_USE_TLS=True
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
