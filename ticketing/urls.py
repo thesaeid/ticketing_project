9278aaa1bca4dc6a1daf00e8840f1fc9cc598875
Erfan <erfankiani10@gmail.com>
2022-07-02 11:48:41 +0430

    user_tickets url implementation

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -4,7 +4,7 @@ from .views import index, submit_ticket, user_tickets, admin_view_ticket,admin_v
 urlpatterns = [
     path("", index),
     path("submit_ticket/", submit_ticket),
-    path("user_tickets/", user_tickets),
+    path("user_tickets/<int:pk>", user_tickets),
     path("admin_view_all_tickets/<int:pk>", admin_view_all_tickets),
     path('admin_view_ticket/<int:ticket_id>', admin_view_ticket),
 ]
